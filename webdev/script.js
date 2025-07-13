6e1b9fed6b81451daaa1a19206a32314e5d51934
adiux06 <coderadi08@gmail.com>
2025-07-13 20:20:45 +0530

    jss

---
+++ webdev/script.js
@@ -0,0 +1,7 @@
+let x; // create a variable but assign it no value
+
+console.log(`x's value is ${x}`); // logs "x's value is undefined"
+
+
+x = 5; // assign a value to x
+console.log(`x's value is now ${x}`); // logs "x's value is 5"
